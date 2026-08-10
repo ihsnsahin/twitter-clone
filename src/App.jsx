bc8726cb274ec16e1cf554393ae2a740b3071ece
ihsnsahin <ihsansahin777@gmail.com>
2026-08-10 11:18:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import Login from './components/Login'
+import Register from './components/Register'
+import HomePage from './components/HomePage'
+
+function App() {
+  return (
+    <>
+      <h1>TEST</h1>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch></>
+
+  )
+}
+
+export default App
